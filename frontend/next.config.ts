51e2954169c0dba722ae0757e74d11df5c9f0203
pranjal kwatra <kwatrapranjal@gmail.com>
2026-09-09 11:35:04 +0530

    Update Next.js configuration for export and paths

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/MindMirror",
+  assetPrefix: "/MindMirror/",
 };
 
 export default nextConfig;
